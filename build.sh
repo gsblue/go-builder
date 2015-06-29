8c2b01a740fba3643d9327b8d8ac46c597415f33
Gurnam Madan <gs.madan@gmail.com>
2015-06-29 22:54:00 +1000

    fixed build script to get the application name from the package

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ fi
 
 echo "Building $pkgName"
 #Grab the application name
-appName = basename $pkgName
+appName=$(basename $pkgName)
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -o ./output/$appName
 
